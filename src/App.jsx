966ce0e8af6e66b4e4de54d6752053b262314e8a
Suteemon <suteemon.team@newton.ac.th>
2023-10-05 23:04:43 +0700

    reset workflow & able to get timetable (still need to fix alignment)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
 import './index.css';
-import { Link, Routes, Route, BrowserRouter as Router} from 'react-router-dom'; 
+import { useParams, Link, Routes, Route, BrowserRouter as Router} from 'react-router-dom'; 
 import HomePage from './components/HomePage';
-import Year10 from './components/years/Year10';
-import Year11 from './components/years/Year11';
-import Year12 from './components/years/Year12';
-import Year13 from './components/years/Year13';
-import YearList from './components/years/YearList';
-// import { BubblyContainer, BubblyLink } from "react-bubbly-transitions";
+import YearHome from './YearHome';
+import YearSchedule from './YearSchedule';
 
 
 
 function App() {
   return(
-      <Router>
+    <Router>
         {/* <BubblyContainer /> */}
           <nav className='pt-4 pl-4'>
           <Link to="/years"> <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
           </svg>
     </Link>
-          {/* <Link to="years">Year 10</Link> */}
       </nav>
       <Routes>
           <Route path='/' element={<HomePage/>} />
-          <Route path='/years' element={<YearList/>} >
-              <Route path='y10' element={<Year10/>}>
-                <Route path=':roomId' element={<Year10/>} />
-                </Route>  
-              <Route path='y11' element={<Year11/>} >
-                <Route path=':roomId' element={<Year11/>} />
-                </Route>
-              <Route path='y12' element={<Year12/>} >
-                <Route path=':roomId' element={<Year12/>} />
-                </Route>
-              <Route path='y13' element={<Year13/>} >
-                <Route path=':roomId' element={<Year13/>} />
-                </Route>
-          </Route>
+          <Route path='/years/*' element={<YearHome/>} />
+          <Route path='/years/year/:yearId' element={<YearSchedule/>} />
       </Routes>
-      </Router>
+    </Router>
   )
 }
+
 
 export default App;
---
+++ src/YearHome.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
+
+function YearHome() {
+    const [years, setYears] = useState([])
+
+    async function getYearArray(){
+        const response = await axios('https://school-management-api.xeersoft.co.th/api/timetable/allyesr') 
+        const responseJson = await response.data
+        setYears(responseJson);
+    }
+    
+    useEffect(() => {
+        getYearArray();
+    },[])
+
+    const yearElements = years
+        .filter((year) => year.lv_tt_link !== null)
+        .map((year) => {
+            return (
+                <div key={year.lv_tt_code}>
+                    <div className=''>
+                        <Link to={`year/${year.lv_tt_code}`} className=" text-lg text-[#032654]  ">
+                            <div className="rounded-lg overflow-hidden transition duration-300 ease-in-out hover:scale-110 ">
+                                <div key={year.lv_title} className=" rounded-lg items-stretch bg-[#D0D7DC] backdrop-blur-lg m-0.5 ">
+                                    <div className="p-4 text-center ">
+                                        {year.lv_title}
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            )
+        })
+
+    return(
+        <>
+        <div className="py-10 text-center">
+            <h1 className="m text-4xl font-bold text-[#032654]">TimeTabs :P</h1>
+            <h3 className="mb-8 text-xl font-bold text-[#47555e]">Select Your Year</h3>
+            <div className="grid grid-cols-2 gap-4 ml-6 mr-6 mb-2">
+            {yearElements}
+            </div>
+        </div>
+        <div className=" mb-12 text-[#032654] text-sm font-semibold text-center">
+        <span>
+            Made with ♡ by Teamm | <a href="https://instagram.com/teammteam"> :D</a>
+        </span>
+        </div>
+        </>
+    )
+
+    
+}
+export default YearHome
