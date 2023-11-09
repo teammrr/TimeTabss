a01968d6fb922c73f949c457b94379312bf92e35
Suteemon <suteemon.team@newton.ac.th>
2023-11-09 10:31:06 +0700

    added loading anim on years page

--- src/YearHome.jsx
+++ src/YearHome.jsx
@@ -6,16 +6,22 @@ import Footer from "./components/footer";
 function YearHome() {
   // var proxy = 'https://cors-anywhere.herokuapp.com/'
   const [years, setYears] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const url =
+    "https://corsproxy.io/?" +
+    encodeURIComponent(
+      "https://school-management-api.xeersoft.co.th/api/timetable/allyesr"
+    );
 
   async function getYearArray() {
-    const url =
-      "https://corsproxy.io/?" +
-      encodeURIComponent(
-        "https://school-management-api.xeersoft.co.th/api/timetable/allyesr"
-      );
-    const response = await axios(url);
-    const responseJson = await response.data;
-    setYears(responseJson);
+    try {
+      const response = await axios(url);
+      const responseJson = await response.data;
+      setYears(responseJson);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   useEffect(() => {
@@ -53,7 +59,33 @@ function YearHome() {
         <h3 className="mb-8 text-xl font-bold text-[#47555e]">
           Select Your Year
         </h3>
-        <div className="grid grid-cols-2 gap-4 ml-6 mr-6">{yearElements}</div>
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-10 w-10 text-[#032654]"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm12 0a8 8 0 00-8-8V0c6.627 0 12 5.373 12 12h-4zm-4 4a4 4 0 110-8 4 4 0 010 8z"
+              ></path>
+            </svg>
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className="grid grid-cols-2 gap-4 ml-6 mr-6">{yearElements}</div>
+        )}
       </div>
       <Footer />
     </>
