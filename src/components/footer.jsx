0792faacfaa0ed19dc5ed5451fe9ffac678066cd
Suteemon <suteemon.team@newton.ac.th>
2023-11-22 10:37:32 +0700

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,23 @@
 export default function Footer() {
   return (
-    <footer className="">
+    <footer>
       <div className="mx-auto max-w-screen-xl pl-4 pr-4 py-4 lg:py-8">
-        <hr className="my-2 border-gray-500 sm:mx-auto lg:my-4" />
-        <div className="sm:flex sm:items-center sm:justify-between  ">
-          <div className="grid grid-cols-2 ">
-            <span className="text-sm text-gray-800 sm:text-center  ">
-              {" "}
-              Crafted with ❤️ by <br />
-              <a href="https://teamrr.live/" className="hover:underline">
-                Teamrr
-              </a>
-            </span>
+        <hr className="my-2 border-gray-200 sm:mx-auto lg:my-4" />
+        <div className="">
+          <div className="flex justify-between">
+            <div>
+              <span className="text-sm text-gray-500 sm:text-center  ">
+                {" "}
+                Crafted with ❤️ by <br />
+                <a href="https://teamrr.live/" className="hover:underline">
+                  Teamrr
+                </a>
+              </span>
+            </div>
             <div className="flex items-center justify-end pr-2">
               <a
-                href="https://teamrr.live/"
-                className="text-gray-800 hover:text-gray-900 "
+                href="https://teamrr.live"
+                className="text-gray-500 hover:text-gray-900 "
               >
                 <svg
                   className="w-4 h-4 "
